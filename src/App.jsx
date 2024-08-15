553f2909c5f345af49af80887cd6cf0b36f38cf4
AshishLawaju <ashish.lawaju74@gmail.com>
2024-08-15 12:23:45 +0545

    build 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ const App = () => {
     <div className="text-4xl p-10">
       <div className="text-[22px]">hello world</div>
       App{" "}
+
+      <div>
+        build 2
+      </div>
     </div>
   );
 };
