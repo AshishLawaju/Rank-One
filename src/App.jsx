38b306696dbb814e6fddd5fce39548f758ffc48e
AshishLawaju <ashish.lawaju74@gmail.com>
2024-08-15 12:21:49 +0545

    ci cd

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="text-4xl p-10">
+      <div className="text-[22px]">hello world</div>
+      App{" "}
+    </div>
+  );
+};
+
+export default App;
